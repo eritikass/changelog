67674dc4637a3e982e972f0dd504db7aee10e719
Siim Tiilen <eritikass@gmail.com>
2018-04-21 00:14:06 +0300

    add render (dummy)

---
+++ packages/changelog-render/render.js
@@ -0,0 +1,13 @@
+const Handlebars = require('handlebars');
+
+// https://www.npmjs.com/package/handlebars
+const source = "<p>Hello, my name is {{name}}. I am from {{hometown}}. I have " +
+  "{{kids.length}} kids:</p>" +
+  "<ul>{{#kids}}<li>{{name}} is {{age}}</li>{{/kids}}</ul>";
+const template = Handlebars.compile(source);
+
+const data = { "name": "Alan", "hometown": "Somewhere, TX",
+  "kids": [{"name": "Jimmy", "age": "12"}, {"name": "Sally", "age": "4"}]};
+const result = template(data);
+
+console.log('result', result);
