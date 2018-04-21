ea8991c51d2df2dbb6a654cbbeb50a3e10a649d0
Pavel Krõlov <pavel.krolov@ekspressdigital.eu>
2018-04-21 20:37:27 +0300

    Add graphql query for comment reaction to pull request

--- packages/changelog-bot/index.js
+++ packages/changelog-bot/index.js
@@ -20,14 +20,25 @@ query getPullRequestInfo($owner: String!, $repository: String!, $pullRequest: In
 }
 `
 
-module.exports = robot => {
-    robot.on('pull_request', async context => {
-        //console.log(context.payload);
+const addComment = `
+mutation addComment($id: ID!, $body: String!) {
+  addComment(input: {subjectId: $id, body: $body, clientMutationId: "AutoChanges"}) {
+    clientMutationId
+  }
+}
+`
 
+module.exports = robot => {
+    robot.on('pull_request.opened', async context => {
         const resource  = await context.github.query(getPullRequestInfo, {
             "owner": context.payload.repository.owner.login,
             "repository": context.payload.repository.name,
             "pullRequest": context.payload.pull_request.number
+        })
+
+        await context.github.query(addComment, {
+            id: resource.repository.pullRequest.id,
+            body: 'AutoChanages bot checked you pull request, preparing Changelog updates!'
         })
 
         console.log(resource);
