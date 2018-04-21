3ac78f42ae7f81412d95f4a541e0ebc2619b7c8e
Siim Tiilen <eritikass@gmail.com>
2018-04-22 00:13:22 +0300

    move infra files

--- gcp-deploy.sh
+++ gcp-deploy.sh
@@ -8,10 +8,10 @@ docker push eritikass/changelog:latest
 # kubectl apply -f kubernetes/config.yaml
 # docker run --rm -e "APP_ID=11336" -e "WEBHOOK_SECRET=Development" -e "PRIVATE_KEY=${PRIVATE_KEY}" -p "8080:8080" eritikass/changelog
 
-kubectl apply -f kubernetes/deployment.yaml
-kubectl apply -f kubernetes/service.yaml
-#kubectl apply -f packages/changelog-gcp/certificate.yaml
-kubectl apply -f kubernetes/ingress.yaml
+kubectl apply -f infra/deployment.yaml
+kubectl apply -f infra/service.yaml
+#kubectl apply -f infra/certificate.yaml
+kubectl apply -f infra/ingress.yaml
 
 
 
