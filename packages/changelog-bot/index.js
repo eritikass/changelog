21a3bd76aada896a899d31ef1212f0720977c73a
Pavel Krõlov <pavel.krolov@ekspressdigital.eu>
2018-04-21 16:46:04 +0300

    Update changelog-bot

--- packages/changelog-bot/index.js
+++ packages/changelog-bot/index.js
@@ -1,17 +1,25 @@
-const createScheduler = require('probot-scheduler')
-
-module.exports = async robot => {
-    const scheduler = createScheduler(robot)
-
-    const events = [
-        'pull_request',
-    ]
-
-    robot.on(events, foobar)
-
-    async function foobar (context) {
-        if (!context.isBot) {
-            context.log('test');
-        }
+const getPullRequestInfo = `
+query getPullRequestInfo($owner: String!, $repository: String!, $pullRequest: Int!) {
+  repository(owner: $owner, name: $repository) {
+    pullRequest(number: $pullRequest) {
+      id
+      number
+      title
     }
+  }
+}
+`
+
+module.exports = robot => {
+    robot.on('*', async context => {
+        console.log(context.payload);
+
+        const { resource } = await context.github.query(getPullRequestInfo, {
+            "owner": context.payload.repository.owner.login,
+            "repository": context.payload.repository.name,
+            "pullRequest": context.payload.pull_request.number
+        })
+
+        console.log(resource);
+    })
 }
