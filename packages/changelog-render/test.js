256efb338c6574c951210d906dc1915e52c8adaa
MetsThe2 <fedya101@gmail.com>
2018-04-21 21:13:41 +0300

    fix pull urls in markdown

--- packages/changelog-render/test.js
+++ packages/changelog-render/test.js
@@ -1,17 +1,6 @@
 const Render = require('./render.js');
 
-
-/* Render.get_tags_commits('./tmp_repos/git-js').then((tags_commits) => {
-    for (var i = 0; i < tags_commits.length; ++i) {
-        console.log(tags_commits[i].tag);
-        for (var j = 0; j < tags_commits[i].commits.length; ++j) {
-            const hash = tags_commits[i].commits[j].hash;
-            if (hash == '2bc60762b6379c85ca202ded061af2ff45f6a5ad') console.log("FOUND");
-        }
-    }
-});*/
-
-var repos = [['steveukx', 'git-js'], ['facebook', 'jest'], ['eventum', 'eventum']]
+var repos = [['facebook', 'jest'], ['eventum', 'eventum']]
 var repo_owner, repo_name;
 [repo_owner, repo_name] = repos[1];
 const md_promise = Render.get_repo_changelog(repo_owner, repo_name);
