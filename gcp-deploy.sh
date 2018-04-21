9e1367f487dee7a0cd3dd78e0efea81e7c65e1d4
Siim Tiilen <eritikass@gmail.com>
2018-04-21 12:47:59 +0300

    use letsencrypt https

--- gcp-deploy.sh
+++ gcp-deploy.sh
@@ -2,6 +2,12 @@
 
 kubectl apply -f packages/changelog-gcp/deployment.yaml
 kubectl apply -f packages/changelog-gcp/service.yaml
+#kubectl apply -f packages/changelog-gcp/certificate.yaml
+kubectl apply -f packages/changelog-gcp/ingress.yaml
 
-# get deployment info
-kubectl get service keep-a-changelog
+
+
+## get deployment info
+#kubectl get service keep-a-changelog
+## get ingress info
+#kubectl get ingress keep-a-changelog
