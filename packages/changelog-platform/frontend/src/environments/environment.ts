1b482633b197260f7cdfcf52f1566d92ea983bb1
Siim Tiilen <eritikass@gmail.com>
2018-04-22 11:15:49 +0300

    use all live urls

--- packages/changelog-platform/frontend/src/environments/environment.ts
+++ packages/changelog-platform/frontend/src/environments/environment.ts
@@ -5,5 +5,5 @@
 
 export const environment = {
   production: false,
-  api: 'http://localhost:8080'
+  api: 'http://autochanges.eu'
 };
--- packages/changelog-platform/server/src/config/github.ts
+++ packages/changelog-platform/server/src/config/github.ts
@@ -1,5 +1,5 @@
 export const CONFIG = {
     client_id: process.env.CLIENT_ID || '623982830e4b37cdb4a7',
     client_secret: process.env.CLIENT_SECRET || 'a1b8be569be2072e46ab884fbfee0c63a65d02bb',
-    oauth_url: process.env.OAUTH_URL || 'http://localhost:8080/api/github/onwebhook',
+    oauth_url: process.env.OAUTH_URL || 'http://autochanges.eu/api/github/onwebhook',
 };
